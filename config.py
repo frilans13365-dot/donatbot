7c9d412573641608daf6fd15ad761ef4b4cc5d13
frilans13365-dot <frilans13365@gmail.com>
2026-04-28 10:38:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+PAYMENTO_API_KEY = os.getenv("PAYMENTO_API_KEY")
+PAYMENTO_WEBHOOK_SECRET = os.getenv("PAYMENTO_WEBHOOK_SECRET")
+BASE_URL = os.getenv("BASE_URL")
+WEBHOOK_PORT = int(os.getenv("WEBHOOK_PORT", 8000))
+
+# Константы
+PAYMENTO_INVOICE_URL = "https://api.paymento.io/v1/invoice"  # Уточните в документации
