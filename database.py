30c6662bb212cfdd9f6765821eef6653e0d37704
frilans13365-dot <frilans13365@gmail.com>
2026-05-06 08:41:57 +0300

    Update database.py

--- database.py
+++ database.py
@@ -10,7 +10,10 @@ _pool = None
 async def get_pool():
     global _pool
     if _pool is None:
-        _pool = await asyncpg.create_pool(config.DATABASE_URL)
+        _pool = await asyncpg.create_pool(
+            config.DATABASE_URL,
+            statement_cache_size=0
+        )
     return _pool
 
 
